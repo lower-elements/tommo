f3b30385ff48f29b1d8c398817724ae717e37cb0
Michael Connor Buchan <mikey@blindcomputing.org>
2022-04-09 15:46:38 +0100

    Implement a very basic TCP listener that accepts connections and spawns tasks.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
-use tokio::io::{self, AsyncWriteExt};
+use std::net::SocketAddr;
+
+use eyre::WrapErr;
+use tokio::{io::AsyncWriteExt, net::{TcpListener, TcpStream}};
+
+const BIND_ADDR: &str = "0.0.0.0:7878";
+
 #[tokio::main]
-async fn main() {
-    let mut stdout = io::stdout();
-    stdout.write_all("Hello, world!\n".as_bytes()).await.unwrap();
-    stdout.flush().await.unwrap();
+async fn main() -> eyre::Result<()> {
+    let listener = TcpListener::bind(BIND_ADDR).await.wrap_err_with(|| format!("Could not bind to address: {}", BIND_ADDR))?;
+    loop {
+        let (mut conn, addr) = listener.accept().await.wrap_err("Could not accept connection")?;
+        tokio::spawn(handle_connection(conn, addr));
+    }
+}
+
+async fn handle_connection(mut conn: TcpStream, addr: SocketAddr) {
+    eprintln!("{} has connected", addr);
+    conn.write_all(b"Hello, world!\n").await.unwrap();
+    conn.flush().await.unwrap();
 }
